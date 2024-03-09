74e9edfa96ba881f2a862cff63f1358e04643da7
Wahaj <154616360+Wajii-Devp@users.noreply.github.com>
2024-03-09 21:01:24 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+let wahajPocketMoney = prompt("Wahaj Pocket Money");
+let moizPocketMoney = prompt("Moiz Pocket Money");
+let muddassirPocketMoney = prompt("Muddassir Pocket Money");
+
+console.log("Wahaj Pocket Money: " + wahajPocketMoney);
+console.log("Moiz Pocket Money: " + moizPocketMoney);
+console.log("Muddassir Pocket Money: " + muddassirPocketMoney);
+console.log(
+  "Total:" + (+wahajPocketMoney + +moizPocketMoney + +muddassirPocketMoney)
+);
